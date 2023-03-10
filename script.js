df4892785bb9e5f21a7b48fd72b72de301738522
jhondev <125105950+Lea1dev@users.noreply.github.com>
2023-03-10 18:18:25 +0000

    Style

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function exibirNome() {
  var nome = document.getElementById("nome").value;
  nome = nome.replace(/ \s+/g, ' ').trim();
  nome = nome.split(" ");
+var outroNome = nome.slice(1);
  document.getElementById('output1').value = nome[0];
- var outroNome = nome.slice(1);
  document.getElementById('output2').value = outroNome.join(" ");
   }
